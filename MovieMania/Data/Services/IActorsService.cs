b4a65d1a62b58f70975ad3c88a898e6bf06e0376
Suvechha Roy <suvechharoy06@outlook.com>
2023-01-12 21:30:14 +0530

    Navbar Design

---
+++ MovieMania/Data/Services/IActorsService.cs
@@ -0,0 +1,14 @@
+﻿using MovieMania.Models;
+
+namespace MovieMania.Data.Services
+{
+    public interface IActorsService
+    {
+        Task<IEnumerable<Actor>> GetAll();
+        Actor GetById(int id);
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+
+    }
+}
